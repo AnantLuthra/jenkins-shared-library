eb2c035a8735b4cbce8b310786a2e3649694e544
Anant <anant312004@gmail.com>
2026-07-30 21:41:22 +0530

    Check

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -18,6 +18,7 @@ class Docker implements Serializable {
     def dockerLogin(){
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
             script.sh '''
+                echo "PASSWORD length: ${#PASSWORD}"
                 echo "$PASSWORD" | docker login -u "$USERNAME" --password-stdin
             '''
         }
