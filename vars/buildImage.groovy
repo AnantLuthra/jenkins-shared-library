449fd8d5823a050501078ad13f5dbacc74b29674
Anant <anant312004@gmail.com>
2026-07-30 16:48:36 +0530

    Added parameters

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "Building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh 'docker build -t anantluthra/simple-java-app:2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh 'docker push anantluthra/simple-java-app:2.0'
+        sh "docker push $imageName"
     }
 }
