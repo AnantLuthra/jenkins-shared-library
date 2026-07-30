aa5689457fbcd1b1a308f97ffe7a6c3b4b95e996
Anant <anant312004@gmail.com>
2026-07-30 19:32:37 +0530

    refactor: restructure shared library into vars and src

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,27 @@
+#!/user/bin/env groovy
+
+package com.example
+
+class Docker implements Serializable {
+    
+    def script
+
+    Docker (script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName){
+        script.echo "Building the docker image..."
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin(){
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+            script.sh "echo '${script.PASSWORD} | docker login -u '${script.USERNAME}' --password-stdin"
+        }
+    }
+
+    def dockerPush(String imageName){
+        script.sh "docker push $imageName"
+    }
+}
